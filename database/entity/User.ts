9ee1c70fa71b399babedc23b93e35370e6971c41
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-30 22:21:30 +0100

    feat: user entity not has avatar column

--- database/entity/User.ts
+++ database/entity/User.ts
@@ -1,4 +1,10 @@
-import { PrimaryGeneratedColumn, Column, BaseEntity, Entity } from "typeorm";
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Entity,
+  BeforeInsert,
+} from "typeorm";
 
 @Entity("users")
 export class User extends BaseEntity {
@@ -14,8 +20,16 @@ export class User extends BaseEntity {
   @Column()
   password: string;
 
+  @Column({ nullable: true }) // should never be null because of @BeforeInsert always sets a default avatar.
+  avatar: string;
+
   @Column({
     unique: true,
   })
   slug: string;
+
+  @BeforeInsert()
+  setDefaultAvatar() {
+    this.avatar = `https://ui-avatars.com/api/?name=${this.username}&color=7F9CF5&background=EBF4FF`;
+  }
 }
