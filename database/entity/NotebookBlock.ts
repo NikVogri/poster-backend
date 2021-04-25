18c0e10589402848e651bdcfe2393068884769ba
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-04-25 23:08:27 +0200

    feat: new db entities

---
+++ database/entity/NotebookBlock.ts
@@ -0,0 +1,46 @@
+import {
+	BaseEntity,
+	BeforeInsert,
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryColumn,
+	UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+import { Notebook } from "./Notebook";
+
+enum NotebookBlockType {
+	image = "image",
+	richText = "richText",
+	code = "code",
+}
+
+@Entity("notebookblocks")
+export class NotebookBlock extends BaseEntity {
+	@PrimaryColumn()
+	id: string;
+
+	@Column("simple-json", { nullable: true })
+	content: string;
+
+	@Column({ nullable: false })
+	type: NotebookBlockType;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@ManyToOne(() => Notebook, (notebook) => notebook.blocks)
+	@JoinColumn({ name: "notebookId" })
+	notebook: Notebook;
+
+	@BeforeInsert()
+	setId() {
+		this.id = uuid();
+	}
+}
---
+++ database/entity/TodoItem.ts
@@ -0,0 +1,43 @@
+import {
+	BaseEntity,
+	BeforeInsert,
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	PrimaryColumn,
+	UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+import { Todo } from "./Todo";
+
+@Entity("todoitems")
+export class TodoItem extends BaseEntity {
+	@PrimaryColumn()
+	id: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@Column()
+	text: string;
+
+	@Column({ default: false })
+	completed: boolean;
+
+	@Column()
+	todoId: string;
+
+	@ManyToOne(() => Todo, (todo) => todo.items)
+	@JoinColumn({ name: "todoId" })
+	todo: Todo;
+
+	@BeforeInsert()
+	setId() {
+		this.id = uuid();
+	}
+}
