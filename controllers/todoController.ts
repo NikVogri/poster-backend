a27a6a71649726cca769770eea199ec7d8431a39
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-11 23:49:52 +0200

    feat: fully functioning todo list backend

--- controllers/todoController.ts
+++ controllers/todoController.ts
@@ -6,16 +6,26 @@ import BadRequestError from "../errors/BadRequestError";
 import NotFoundError from "../errors/NotFoundError";
 import { Todo } from "../database/entity/Todo";
 import ServerError from "../errors/ServerError";
+import { TodoItem } from "../database/entity/TodoItem";
+import { TodoItemUpdate } from "../interfaces/todoInterface";
 
 export const addNewBlock = async (
 	req: UserPageRequest,
 	res: Response,
 	next: NextFunction
 ) => {
-	const { id } = req.params;
-	const { title, headerColor } = req.body;
-
 	try {
+		const { id } = req.params;
+		const { title, headerColor } = req.body;
+
+		if (typeof title !== "string" || title.trim() === "") {
+			throw new BadRequestError("Please provide a title");
+		}
+
+		if (typeof headerColor !== "string" || title.trim() === "") {
+			throw new BadRequestError("Please provide header color");
+		}
+
 		const page = await Page.findOne({ id }, { relations: ["todos"] });
 		const todo = await Todo.create({ title, headerColor }).save();
 
@@ -38,7 +48,123 @@ export const addNewBlock = async (
 		next(err);
 	}
 };
-export const updateBlock = (req: Request, res: Response) => {};
+export const updateBlock = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { todoId } = req.params;
+		const { title, headerColor } = req.body;
 
-export const addTask = (req: Request, res: Response) => {};
-export const toggleTask = (req: Request, res: Response) => {};
+		if (typeof title !== "string" || title.trim() === "") {
+			throw new BadRequestError("Please provide a title");
+		}
+
+		if (typeof headerColor !== "string" || headerColor.trim() === "") {
+			throw new BadRequestError("Please provide header color");
+		}
+
+		const todoBlock = await Todo.findOne(
+			{ id: todoId },
+			{ relations: ["items"] }
+		);
+
+		if (!todoBlock) {
+			throw new BadRequestError("Todo not found");
+		}
+
+		todoBlock.title = title;
+		todoBlock.headerColor = headerColor;
+
+		await todoBlock.save();
+
+		res.status(200).send({ success: true, todoBlock });
+	} catch (err) {
+		console.log(err);
+		next(err);
+	}
+};
+
+export const addTask = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { todoId } = req.params;
+		const { text } = req.body;
+
+		if (typeof text !== "string" || text.trim() === "") {
+			throw new BadRequestError("Please provide todo item text");
+		}
+
+		const todoBlock = await Todo.findOne(
+			{ id: todoId },
+			{ relations: ["items"] }
+		);
+
+		if (!todoBlock) {
+			throw new BadRequestError("Todo block not found");
+		}
+
+		const todoItem = await TodoItem.create({ text }).save();
+
+		todoBlock.items.push(todoItem);
+		await todoBlock.save();
+
+		res.status(200).send({ success: true, todoItem });
+	} catch (err) {
+		console.log(err);
+		next(err);
+	}
+};
+
+export const updateTask = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { updateType, taskId } = req.body;
+
+		if (typeof taskId !== "string" || taskId.trim() === "") {
+			throw new BadRequestError("Please provide taskId");
+		}
+
+		if (
+			!updateType ||
+			!Object.values(TodoItemUpdate).includes(updateType)
+		) {
+			throw new BadRequestError("Please provide correct updateType");
+		}
+
+		const task = await TodoItem.findOne({ id: taskId });
+
+		if (!task) {
+			throw new BadRequestError("Please provide correct taskId");
+		}
+
+		switch (updateType) {
+			case TodoItemUpdate.COMPLETE:
+				task.completed = true;
+				await task.save();
+				break;
+			case TodoItemUpdate.UNCOMPLETE:
+				task.completed = false;
+				await task.save();
+				break;
+			case TodoItemUpdate.REMOVE:
+				await task.remove();
+				break;
+		}
+
+		res.status(200).send({
+			success: true,
+			todoItem: updateType === TodoItemUpdate.REMOVE ? null : task,
+		});
+	} catch (err) {
+		console.log(err);
+		next(err);
+	}
+};
